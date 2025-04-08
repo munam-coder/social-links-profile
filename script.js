0ca251f1e0a649e4fab7886fb1778e9256f33137
Abdul Munam <abdulmunam791@gmail.com>
2025-04-09 03:26:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let btn1 = document.getElementById("btn1")
+
+btn1.addEventListener("click", ()=>{
+    window.location.href = "https://github.com/"
+    
+})
+
+let btn2 = document.getElementById("btn2")
+
+btn2.addEventListener("click", ()=>{
+    window.location.href = "https://www.frontendmentor.io/"
+})
+
+let btn3 = document.getElementById("btn3")
+
+btn3.addEventListener("click", ()=>{
+    window.location.href = "https://pk.linkedin.com/"
+})
+let btn4 = document.getElementById("btn4")
+
+btn4.addEventListener("click", ()=>{
+    window.location.href = "https://x.com/?lang=en"
+})
+let btn5 = document.getElementById("btn5")
+
+btn5.addEventListener("click", ()=>{
+    window.location.href = "https://www.instagram.com/"
+})
